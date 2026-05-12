549389a0d9f5797f920a4d80149ef0ae115e48e5
lee <nhattruongta13@gmail.com>
2026-05-12 16:54:54 -0500

    Convert submodule to normal folder

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors'
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+
+
+
+app.listen(PORT, () => {
+    console.log('The server is live baby')
+})
+
