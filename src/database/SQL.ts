fc4265db25cf769507c20baf48a450c17fb58a7e
lee <nhattruongta13@gmail.com>
2026-05-12 18:14:30 -0500

    change something

--- src/database/SQL.ts
+++ src/database/SQL.ts
@@ -1,6 +1,6 @@
 import {pool} from './db'
 
 export const create = (() => pool.query(`CREATE TABLE users(
-    id: SERIAL PRIMARY KEY,
-    email: TEXT UNIQUE NOT NULL,
-    password: TEXT NOT NULL)`))
+    id: serial PRIMARY KEY,
+    email: text UNIQUE NOT NULL,
+    password: text NOT NULL)`))
