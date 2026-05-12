525acab6c4266344660f8801777f35f9acac7606
lee <nhattruongta13@gmail.com>
2026-05-12 18:24:12 -0500

    change

--- src/database/SQL.ts
+++ src/database/SQL.ts
@@ -4,4 +4,4 @@ export const create = (() => pool.query(`CREATE TABLE users(
     Id SERIAL PRIMARY KEY,
     Email TEXT UNIQUE NOT NULL,
     Password_hash TEXT NOT NULL,
-    Time DATE TIMESTAMPTZ.NOW())`))
+    Time TIMESTAMPTZ NOW())`))
