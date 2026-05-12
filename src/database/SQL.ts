7444ea7ee87ea0b5ab79d85b009b3e84c919a179
lee <nhattruongta13@gmail.com>
2026-05-12 18:12:28 -0500

    forget comma

--- src/database/SQL.ts
+++ src/database/SQL.ts
@@ -2,5 +2,5 @@ import {pool} from './db'
 
 export const create = (() => pool.query(`CREATE TABLE users(
     id: SERIAL PRIMARY KEY,
-    email: TEXT UNIQUE NOT NULL
+    email: TEXT UNIQUE NOT NULL,
     password: TEXT NOT NULL)`))
