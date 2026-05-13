ddce68c45439084bdeefe7a941e6a45769627941
lee <nhattruongta13@gmail.com>
2026-05-13 11:43:51 -0500

    change tsconfig

--- src/route/auth.ts
+++ src/route/auth.ts
@@ -2,7 +2,7 @@ import {register} from '../services/db_manip'
 import {Router, Request, Response, NextFunction} from 'express'
 import {errorHandler} from '../middlewares/errorHandler'
 import {UserSchema} from '../schemas/validation'
-import {generateRequestId} from '../middlewares/req_config.d'
+import {generateRequestId} from '../middlewares/req_config'
 
 
 const router = Router()
