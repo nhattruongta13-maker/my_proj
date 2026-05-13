f890e24893dfabac8aa12dc7c2e8d0a7331c27ca
lee <nhattruongta13@gmail.com>
2026-05-13 13:10:06 -0500

    return row

--- src/database/SQL.ts
+++ src/database/SQL.ts
@@ -1,8 +1,9 @@
 import {pool} from './db'
 
 export const insertUser = (async (email: string, password: string) => {
-    await pool.query(`INSERT INTO users (email, password_hash) VALUES ($1, $2)
+    const result = await pool.query(`INSERT INTO users (email, password_hash) VALUES ($1, $2)
                 RETURNING id, email, time`,
                 [email, password]
     )
+    return result
 })
--- src/services/db_manip.ts
+++ src/services/db_manip.ts
@@ -4,5 +4,5 @@ import bcrypt from 'bcrypt'
 export const register = async (email: string, password: string) => {
     const password_hash = await bcrypt.hash(password, 10)
     const newUser = await insertUser(email, password_hash)
-    return newUser
+    return newUser.rows[0]
 }
