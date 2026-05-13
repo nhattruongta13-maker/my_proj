3127acb93a685c991459c7202f89f819a0a0c480
lee <nhattruongta13@gmail.com>
2026-05-13 12:01:29 -0500

    change path

--- src/middlewares/generateRequestId.ts
+++ src/middlewares/generateRequestId.ts
@@ -1,16 +1,7 @@
-import {Logger} from 'pino'
 import {randomUUID} from 'crypto'
 import {logger} from '../lib/logger'
 import {Request, Response, NextFunction} from 'express'
 
-declare global{
-    namespace Express{
-        interface Request{
-            id: string
-            log: Logger
-        }
-    }
-}
 
 export const generateRequestId = ((req: Request, res: Response, next:NextFunction) => {
     req.id = req.headers['x-request-header'] as string || randomUUID()
--- src/route/auth.ts
+++ src/route/auth.ts
@@ -2,7 +2,7 @@ import {register} from '../services/db_manip'
 import {Router, Request, Response, NextFunction} from 'express'
 import {errorHandler} from '../middlewares/errorHandler'
 import {UserSchema} from '../schemas/validation'
-import {generateRequestId} from '../middlewares/req_config'
+import {generateRequestId} from '../middlewares/generateRequestId'
 
 
 const router = Router()
