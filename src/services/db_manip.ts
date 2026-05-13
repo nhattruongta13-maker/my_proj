9405245da7f3df104e38ac3425fcb0a0e2374a5e
lee <nhattruongta13@gmail.com>
2026-05-13 18:25:03 -0500

    ..

--- src/services/db_manip.ts
+++ src/services/db_manip.ts
@@ -10,7 +10,7 @@ export const register = async (email: string, password: string) => {
 export const login = async (email: string, password: string) => {
     const dummyHash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
     const user = await findUser(email)
-    const hashToCompare = user?.rows[0].password_hash?? dummyHash
+    const hashToCompare = user?.rows[0]?.password_hash?? dummyHash
     const valid = await bcrypt.compare(password, hashToCompare)
     return valid
 }
