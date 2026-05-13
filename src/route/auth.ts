c1e69ed200490d264806842fb4293d5aefb745dd
lee <nhattruongta13@gmail.com>
2026-05-13 16:25:24 -0500

    add logs

--- src/route/auth.ts
+++ src/route/auth.ts
@@ -12,11 +12,14 @@ router.use(generateRequestId)
 router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
     try{
     const {email, password} = UserSchema.parse(req.body)
+    req.log.info({req_body: req.body}, 'user.signup.attempt')
     const newUser = await register(email, password)
+    req.log.info({user: newUser}, 'user.signup.success')
     return res.status(201).json({msg: "Account created",
                                  info: newUser
     })
     }catch(err){
+        req.log.error(err, 'user.signup.fail')
         next(err)
     }
 })
