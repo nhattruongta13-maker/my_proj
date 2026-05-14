4c460620c14907eeebf77df4ec82a3628360633d
lee <nhattruongta13@gmail.com>
2026-05-14 01:11:46 -0500

    fix

--- src/validation/createWebToken.ts
+++ src/validation/createWebToken.ts
@@ -3,7 +3,7 @@ import {StringValue} from 'ms'
 
 export const createWebToken = (payload: {id: number}) => {
     const secret = process.env.JWT_SECRET
-    const expiresIn = process.env.JWT_EXPIRES_IN as StringValue || '7d'
+    const expiresIn = '7d'
     if (!secret) {
         throw new Error("JWT_SECRET is missing. Blame yourself!")
     }
