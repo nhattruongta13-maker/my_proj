216c73e2512cc05c9c773ff9259c121587c2cde3
lee <nhattruongta13@gmail.com>
2026-05-14 22:39:39 -0500

    add new error condition

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,6 @@
 import {Request, Response, NextFunction} from 'express'
 import {ZodError} from 'zod'
-import {AuthError} from '../errors/errors'
+import {AuthError, NosyError} from '../errors/errors'
 import jwt from 'jsonwebtoken'
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
@@ -14,6 +14,8 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
         return res.status(401).json({error: 'You left too long'})
     }else if(err instanceof jwt.JsonWebTokenError){
         return res.status(401).json({error: "Stop hacking dude"})
+    }else if(err instanceof NosyError){
+        return res.status(403).json({error: err.message})
     }
     else{
         return res.status(500).json({err: "Something unexpected happened"})
