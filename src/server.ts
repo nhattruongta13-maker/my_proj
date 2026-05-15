e48075c9a7212d71f437e0f9a67c6fa7dce440fb
lee <nhattruongta13@gmail.com>
2026-05-15 03:01:50 -0500

    update trust proxy

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import authRouter from './route/auth'
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.set('trust proxy', true)
+app.set('trust proxy', 1)
 
 const PORT = process.env.PORT
 app.use('/auth', authRouter)
